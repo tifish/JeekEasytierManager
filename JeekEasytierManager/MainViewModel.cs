d150c26c5dc3441d2a74a34d3dea2cbabd28ce3b
Tinyfish <tifish@gmail.com>
2025-07-03 02:41:41 +0800

    Deal with all rpc portal values.

--- JeekEasytierManager/MainViewModel.cs
+++ JeekEasytierManager/MainViewModel.cs
@@ -182,18 +182,41 @@ public partial class MainViewModel : ObservableObject, IDisposable
         Process.Start(new ProcessStartInfo("explorer.exe", configFile) { UseShellExecute = true });
     }
 
-    private static string GetRpcPortal(string configName)
+    private static string GetRpcSocket(string configName)
     {
         var configFile = Path.Combine(AppSettings.ConfigDirectory, configName + ".toml");
         if (!File.Exists(configFile))
             return "";
 
+        const string defaultRpcSocket = "127.0.0.1:15888";
+
         var toml = Toml.ReadFile(configFile);
         var rpcPortal = toml.TryGetValue("rpc_portal");
         if (rpcPortal is null)
-            return "";
+            return defaultRpcSocket;
 
-        return rpcPortal.Get<string>();
+        var rpcPortalString = rpcPortal.Get<string>().Trim();
+
+        if (rpcPortalString == "")
+            return defaultRpcSocket;
+
+        var parts = rpcPortalString.Split(':');
+        if (parts.Length == 1)
+        {
+            if (parts[0] == "0")
+                return defaultRpcSocket;
+            else
+                return $"127.0.0.1:{parts[0]}";
+        }
+        else if (parts.Length == 2)
+        {
+            if (parts[0] == "0.0.0.0")
+                return $"127.0.0.1:{parts[1]}";
+            else
+                return rpcPortalString;
+        }
+
+        return defaultRpcSocket;
     }
 
     [RelayCommand]
@@ -212,10 +235,8 @@ public partial class MainViewModel : ObservableObject, IDisposable
             if (config.Status != ServiceStatus.Running)
                 continue;
 
-            var rpcPortal = GetRpcPortal(config.Name);
-            var args = rpcPortal == "" ? "" : $"-p {rpcPortal}";
-
-            var peers = await Executor.RunWithOutput(AppSettings.EasytierCliPath, $"{args} peer", Encoding.UTF8);
+            var rpcSocket = GetRpcSocket(config.Name);
+            var peers = await Executor.RunWithOutput(AppSettings.EasytierCliPath, $"-p {rpcSocket} peer", Encoding.UTF8);
             Messages += $"{config.Name}:\n{peers}\n\n";
         }
     }
@@ -236,10 +257,8 @@ public partial class MainViewModel : ObservableObject, IDisposable
             if (config.Status != ServiceStatus.Running)
                 continue;
 
-            var rpcPortal = GetRpcPortal(config.Name);
-            var args = rpcPortal == "" ? "" : $"-p {rpcPortal}";
-
-            var route = await Executor.RunWithOutput(AppSettings.EasytierCliPath, $"{args} route", Encoding.UTF8);
+            var rpcSocket = GetRpcSocket(config.Name);
+            var route = await Executor.RunWithOutput(AppSettings.EasytierCliPath, $"-p {rpcSocket} route", Encoding.UTF8);
             Messages += $"{config.Name}:\n{route}\n\n";
         }
     }
