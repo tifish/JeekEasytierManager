81508c4bd32b2e17a90492fc32c057c959955a56
Tinyfish <tifish@gmail.com>
2025-07-14 18:31:11 +0800

    Fix wrong character in code.

--- JeekEasyTierManager/MainWindow/MainViewModel.cs
+++ JeekEasyTierManager/MainWindow/MainViewModel.cs
@@ -12,30 +12,30 @@ public partial class MainViewModel : ObservableObject, IDisposable
     public static MainViewModel Instance { get; private set; } = new();
 
     [ObservableProperty]
-    public partial ObservableCollection<ConfigInfo> Configs% { get; set; } = [];
+    public partial ObservableCollection<ConfigInfo> Configs { get; set; } = [];
 
     public async Task Init()
-{
-    GitHubMirrors.TestUrl = AppSettings.JeekEasyTierManagerZipUrl;
+    {
+        GitHubMirrors.TestUrl = AppSettings.JeekEasyTierManagerZipUrl;
 
-    await AppSettings.Load();
-    LoadConfigs(true);
-    CheckHasEasyTier();
-    await ShowInfo();
-    await ApplySettings();
-}
+        await AppSettings.Load();
+        LoadConfigs(true);
+        CheckHasEasyTier();
+        await ShowInfo();
+        await ApplySettings();
+    }
 
-public void Dispose()
-{
-    _autoUpdateTimer?.Stop();
+    public void Dispose()
+    {
+        _autoUpdateTimer?.Stop();
 
-    GC.SuppressFinalize(this);
-}
+        GC.SuppressFinalize(this);
+    }
 
-private MainWindow? _mainWindow;
+    private MainWindow? _mainWindow;
 
-public void SetMainWindow(MainWindow mainWindow)
-{
-    _mainWindow = mainWindow;
-}
+    public void SetMainWindow(MainWindow mainWindow)
+    {
+        _mainWindow = mainWindow;
+    }
 }
